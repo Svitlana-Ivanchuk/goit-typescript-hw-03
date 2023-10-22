d71c40a9d0dfaa05d671688f13dc0a9c59fa0895
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-10-22 19:33:46 +0300

    add 1.ts, 2.ts, 3.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,20 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person extends Key {
+  private key: Key;
+  getKey() {
+    return key;
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +23,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
