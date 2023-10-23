ad5b684636564696abc0bc2014963ae211e8f625
Svitlana-Ivanchuk <aig.sveta@gmail.com>
2023-10-23 23:34:59 +0300

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -8,10 +8,46 @@ class Key {
   }
 }
 
-class Person extends Key {
-  private key: Key;
-  getKey() {
-    return key;
+class Person {
+  constructor(private key: Key) {
+    //this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+ abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] =[];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door === true) {
+      this.tenants.push(person);
+    }
+  }
+
+  public abstract openDoor(key: Key): void;
+
+  // я не розумію що тут треба зробити, чому підсвічує class
+class MyHouse extends House {
+
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door === true
+    } else { this.door === false }
+      
+    
   }
 }
 
